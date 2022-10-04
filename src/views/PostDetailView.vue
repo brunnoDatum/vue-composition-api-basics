105928976279aca6e3b8534ea70c7dc7ce8fc7f4
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 10:32:02 -0400

    Adding some routes

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <p>Display the content of post ID: {{ $route.params.id }}</p>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+</template>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/3">Post 3</RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
