70de9c074a253afb4c32a7bcface41a8eff3e654
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 16:18:03 -0400

    Migrate modal to its own component

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,27 +2,13 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal">Show Modals</button>
-    <Teleport to=".modals-container">
-      <div class="modal" v-if="visible">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type specimen
-          book. It has survived not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in the 1960s
-          with the release of Letraset sheets containing Lorem Ipsum passages, and more
-          recently with desktop publishing software like Aldus PageMaker including
-          versions of Lorem Ipsum.
-        </p>
-        <button @click="hideModal">Hide Modal</button>
-      </div>
-    </Teleport>
+    <Modal v-if="visible"></Modal>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Modal from "@/components/Modal.vue";
 
 const visible = ref(false);
 
@@ -33,17 +19,3 @@ const hideModal = () => {
   visible.value = false;
 };
 </script>
-
-<style scoped>
-.modal {
-  background: beige;
-  padding: 10px;
-  color: black;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
