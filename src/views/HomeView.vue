9e78fae3e0399133686c467a80a78c11433132c9
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 09:19:09 -0400

    New setup implementation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="home">
+    <h3>{{ title }}</h3>
+
     <div>
       <button class="btn" @click="decreaseCounter">-</button>
       <span class="counter">{{ counter }}</span>
       <button class="btn" @click="increaseCounter">+</button>
     </div>
+
+    <div class="edit">
+      <h4>Edit title:</h4>
+      <input type="text" v-model="title" />
+    </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from "vue";
 
-export default {
-  setup(props) {
-    const counter = ref(0);
+const counter = ref(0);
+const title = ref("Counter:");
 
-    const increaseCounter = () => {
-      counter.value++;
-    };
+const increaseCounter = () => {
+  counter.value++;
+};
 
-    const decreaseCounter = () => {
-      counter.value--;
-    };
-
-    return { counter, increaseCounter, decreaseCounter };
-  },
+const decreaseCounter = () => {
+  counter.value--;
 };
 </script>
 
@@ -37,5 +39,9 @@ export default {
 .btn,
 .counter {
   font-size: 40px;
+}
+
+.edit {
+  margin-top: 60px;
 }
 </style>
