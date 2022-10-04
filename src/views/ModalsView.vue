c80b8c3966425ea81570caf55abc201396dba1e1
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 16:25:39 -0400

    Modal content as slots

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,7 +2,19 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal">Show Modals</button>
-    <Modal v-if="visible"></Modal>
+    <Modal v-if="visible">
+      <template v-slot:title>Modal Title</template>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+        unknown printer took a galley of type and scrambled it to make a type specimen
+        book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+    </Modal>
   </div>
 </template>
 
