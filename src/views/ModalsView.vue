bcf336939d9954109e7e8770f60f01de38680aa6
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 14:02:21 -0400

    Add base modal before teleport feature

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button @click="showModal">Show Modals</button>
+    <div class="modal" v-if="visible">
+      <h1>This is a modal</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+        unknown printer took a galley of type and scrambled it to make a type specimen
+        book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <button @click="hideModal">Hide Modal</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const visible = ref(false);
+
+const showModal = () => {
+  visible.value = true;
+};
+const hideModal = () => {
+  visible.value = false;
+};
+</script>
+
+<style scoped>
+.modal {
+  background: beige;
+  padding: 10px;
+  color: black;
+}
+</style>
