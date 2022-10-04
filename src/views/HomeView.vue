54b57be7dad355be0fce0533f5bf3f40a8ebd7b2
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 08:55:26 -0400

    Applied Composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+      <button class="btn" @click="decreaseCounter">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="increaseCounter">+</button>
+    </div>
   </div>
 </template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup(props) {
+    const counter = ref(0);
+
+    const increaseCounter = () => {
+      counter.value++;
+    };
+
+    const decreaseCounter = () => {
+      counter.value--;
+    };
+
+    return { counter, increaseCounter, decreaseCounter };
+  },
+};
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+}
+</style>
