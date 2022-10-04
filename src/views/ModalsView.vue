af66489d88f1e0c92569fc60fc71aeded98d487f
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-04 15:46:59 -0400

    Used teleport to create modal overlay outside

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,20 +2,22 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal">Show Modals</button>
-    <div class="modal" v-if="visible">
-      <h1>This is a modal</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-        unknown printer took a galley of type and scrambled it to make a type specimen
-        book. It has survived not only five centuries, but also the leap into electronic
-        typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-        the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
-      </p>
-      <button @click="hideModal">Hide Modal</button>
-    </div>
+    <Teleport to=".modals-container">
+      <div class="modal" v-if="visible">
+        <h1>This is a modal</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type specimen
+          book. It has survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s
+          with the release of Letraset sheets containing Lorem Ipsum passages, and more
+          recently with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum.
+        </p>
+        <button @click="hideModal">Hide Modal</button>
+      </div>
+    </Teleport>
   </div>
 </template>
 
@@ -37,5 +39,11 @@ const hideModal = () => {
   background: beige;
   padding: 10px;
   color: black;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 </style>
